7403bd257f59dc6b3d99bca5b9ae981b4945c0cc
mozesi <msukwamose1@gmail.com>
2023-04-29 18:21:15 +0200

    laying out the landing page0

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,51 +1,51 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Pakaya</a>
+      <a class="navbar-brand" href="/">Pakaya</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-          <a class="nav-link" href="#">Features</a>
-          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link" href="#about">About</a>
+          <a class="nav-link" href="#contact">Contact</a>
         </div>
+        <div class="d-flex">
+            @if(Auth::check())
+            <x-dropdown align="right" width="48">
+                <x-slot name="trigger">
+                    <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                        <div>{{ Auth::user()->name }}</div>
+      
+                        <div class="ml-1">
+                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                    </button>
+                </x-slot>
+      
+                <x-slot name="content">
+                    <x-dropdown-link :href="route('profile.edit')">
+                        {{ __('Profile') }}
+                    </x-dropdown-link>
+      
+                    <!-- Authentication -->
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+      
+                        <x-dropdown-link :href="route('logout')"
+                                onclick="event.preventDefault();
+                                            this.closest('form').submit();">
+                            {{ __('Log Out') }}
+                        </x-dropdown-link>
+                    </form>
+                </x-slot>
+            </x-dropdown>
+            @else
+            <a data-bs-toggle="modal" data-bs-target="#registerModal" >Sign Up</a>
+            <a data-bs-toggle="modal" data-bs-target="#loginModal" >Log In</a>
+            @endif
+          </div>
       </div>
     </div>
-    <div class="hidden sm:flex sm:items-center sm:ml-6">
-      @if(Auth::check())
-      <x-dropdown align="right" width="48">
-          <x-slot name="trigger">
-              <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                  <div>{{ Auth::user()->name }}</div>
-
-                  <div class="ml-1">
-                      <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                          <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                      </svg>
-                  </div>
-              </button>
-          </x-slot>
-
-          <x-slot name="content">
-              <x-dropdown-link :href="route('profile.edit')">
-                  {{ __('Profile') }}
-              </x-dropdown-link>
-
-              <!-- Authentication -->
-              <form method="POST" action="{{ route('logout') }}">
-                  @csrf
-
-                  <x-dropdown-link :href="route('logout')"
-                          onclick="event.preventDefault();
-                                      this.closest('form').submit();">
-                      {{ __('Log Out') }}
-                  </x-dropdown-link>
-              </form>
-          </x-slot>
-      </x-dropdown>
-      @else
-      <a data-bs-toggle="modal" data-bs-target="#registerModal" >Sign Up</a>
-      <a data-bs-toggle="modal" data-bs-target="#loginModal" >Log In</a>
-      @endif
-  </div>
   </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,39 +7,50 @@
         <title>Pakaya</title>
 
         <!-- Fonts -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-        <script src="https://cdn.tailwindcss.com"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"  crossorigin="anonymous">
         <!-- Styles -->
         <style>
           </style>
         @livewireStyles
     </head>
     <body class="">
-        @include('layouts.navigation')
         {{-- 
-            Modals for account registration and login
+            including the top navigation and modals for account registration and login
         --}}
+        @include('layouts.navigation')
         @include('layouts.Modals.login')
         @include('layouts.Modals.register')
-        <section class="container mx-auto">
-            <div class="columns-5">
-                    <h1 class="text-3xl font-bold">
-                        Welcome to <span class="text-warning">Pakaya.</span>
-                    </h1>
-                    <p class="">
-                            Enjoy reading and contributing to a variety of Malawian proverbs in your favorite local languages.
-                            You can also share your understanding of a proverb and Up vote someone's insights of a proverb. Have fun!
-                    </p>
-                    <div class="">
-                        <button data-bs-toggle="modal" data-bs-target="#loginModal" class="btn btn-primary">Sign In</button>
-                        <a href="/home/guest" class="btn btn-primary">Proceed as a guest</a>            
-                    </div>
+        <section class="container">
+            <div class="row m-4">
+                <div class="col-8 p-4">
+                        <h1 class="lead">
+                            Welcome to <span class="text-warning">Pakaya.</span>
+                        </h1>
+                        <p class="">
+                                Enjoy reading and contributing to a variety of Malawian proverbs in your favorite local languages.
+                                You can also share your understanding of a proverb and Up vote someone's insights of a proverb. Have fun!
+                        </p>
+                        <div class="">
+                            <button data-bs-toggle="modal" data-bs-target="#loginModal" class="btn btn-primary">Sign In</button>
+                            <a href="/home/guest" class="btn btn-primary">Proceed as a guest</a>            
+                        </div>
+                </div>
+                <div class="col-4">
+                            <img class="img-fluid w-50 d-none d-sm-block" src="{{URL::asset('/img/home_one.png')}}" alt="">
+                </div>
             </div>
-            <div class="columns-6">
-                        <img class="img-fluied w-50 d-none d-sm-block" src="{{URL::asset('/img/home_one.png')}}" alt="">
+
+            <div id="about">
+                 About section
             </div>
+            <div id="contact">
+                contact section
+           </div>
         </section>
+        <div id="footer">
+            Footer section
+       </div>
         @livewireScripts
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"  crossorigin="anonymous"></script>
     </body>
 </html>
