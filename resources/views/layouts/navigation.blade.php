8626eb5c6b75a02f1b7bffc71dc3342c8150cb9b
mozesi <msukwamose1@gmail.com>
2023-05-12 08:17:51 +0200

    designing the home page

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,40 +1,13 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
+    <div class="container">
       <a class="navbar-brand" href="/">Pakaya</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
+      <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
+        <div class="navbar-nav ms-auto">
           <a class="nav-link" href="#about">About</a>
           <a class="nav-link" href="#contact">Contact</a>
         </div>
-        <div class="d-flex">
-            @if(Auth::check())
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  {{ Auth::user()->name }}
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="{{route('profile.edit')}}">{{ __('Profile') }}</a></li>
-                    <!-- Authentication -->
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
-                        <li>
-                        <a class="dropdown-item" href="route('logout')"
-                                onclick="event.preventDefault();
-                                            this.closest('form').submit();">
-                            {{ __('Log Out') }}
-                        </a>
-                    </li>
-                    </form>
-                </ul>
-            </li>
-            @else
-            <a href="{{ route('register') }}" >Sign Up</a>
-            <a href="{{ route('login') }}" >Log In</a>
-            @endif
-          </div>
-      </div>
     </div>
   </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,36 +18,59 @@
             including the top navigation and modals for account registration and login
         --}}
         @include('layouts.navigation')
-        <section class="container">
-            <div class="row m-4">
-                <div class="col-8 p-4">
+        <section class="p-5 text-center text-sm-start" >
+            <div class="container">
+                <div class="d-sm-flex align-items-center" >
+                    <div>
                         <h1 class="lead">
                             Welcome to <span class="text-warning">Pakaya.</span>
                         </h1>
-                        <p class="">
+                        <p class="lead my-4">
                                 Enjoy reading and contributing to a variety of Malawian proverbs in your favorite local languages.
                                 You can also share your understanding of a proverb and Up vote someone's insights of a proverb. Have fun!
                         </p>
                         <div class="">
                             <a href="{{ route('login') }}" class="btn btn-primary">Log In</a>
-                            <a href="/home/guest" class="btn btn-primary">Proceed as a guest</       
+                            <a href="/home/guest" class="btn btn-primary">Proceed as a guest</a>       
                         </div>
-                </div>
-                <div class="col-4">
-                            <img class="img-fluid w-50 d-none d-sm-block" src="{{URL::asset('/img/home_one.png')}}" alt="">
+                    </div>
+                    <img class="img-fluid w-50 d-none d-sm-block" src="{{URL::asset('/img/home_one.png')}}" alt="">
                 </div>
             </div>
-
-            <div id="about">
-                 About section
-            </div>
-            <div id="contact">
-                contact section
-           </div>
         </section>
-        <div id="footer">
-            Footer section
-       </div>
+        {{-- --}}
+        <section class="bg-primary text-light p-5">
+            <div class="container">
+                <div class="d-md-flex jutsify-content-between align-items-center">
+                    <h2>
+                        sign up
+                    </h2>
+                </div>
+            </div>
+        </section>
+        {{-- --}}
+        <section class="p-5">
+            <div class="container">
+                <div class="row text-center">
+                    <div class="col-md">
+                        ONe
+                    </div>
+                    <div class="col-md">
+                        ONe
+                    </div>
+                    <div class="col-md">
+                        ONe
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="p-5">
+            <div class="container">
+                <div class="row align-items">
+                    
+                </div>
+            </div>
+        </section>
         @livewireScripts
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"  crossorigin="anonymous"></script>
     </body>
